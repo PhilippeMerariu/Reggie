1d93c49c93a64fdee38f00d0806909d3993ca090
Evan Greenstein <evanlg16@gmail.com>
2020-10-21 00:04:39 -0400

    Added regex pattern to module.exports fixed config.json

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 const CommandStore = require('./src/commandStore');
 const client = new Discord.Client();
 
+
 commands = CommandStore.commands;
 
 /**
@@ -43,47 +44,42 @@ client.on('message', processMessage);
 	 * Parses message to differentiate between r!command and regular message (eg notch)
 	 * @param msg
 	 */
-	function processMessage (msg) {
-		var message = msg.content.toLowerCase();
-		var channelId = msg.channel.id;
-		var channel = client.channels.cache.get(channelId);
-		
-		var dicePattern = new RegExp('\\d+[d]\\d+'); //search for '#d#' expression.
-		
-		let isBot = msg.author.bot;
-		//command and message handling block
-		//commands that look for keyword
-		if (message.startsWith(keyword) && !isBot){
-			message = message.substring(2); //extracts the message without the "r!"
-			if (message === commands.beer.name) {
-				commands.beer.main(channel);
-			}
-			else if (message === commands.flops.name) {
-				commands.flops.main(channel);
-			}
-			else if (message === commands.drink.name){
-				commands.drink.main(channel);
-			}
-			else if (dicePattern.test(message)){
-				commands.dice.main(channel, message);
-			}
-			else if (message === commands.help.name){
-				var helpBox = new Discord.MessageEmbed();
-				commands.help(channel, helpBox);
-			}
-		}
-		//commands that do NOT look for keyword
-		else if (!isBot) {
-			if (commands.caCourt.caMarche(message)) {
-				commands.caCourt.court(channel);
-			}
-			if (commands.notch.notchable(message)) {
-				commands.notch.main(channel);
-			}
-			if (commands.huh.canHuh(message)) {
-				commands.huh.main(channel, message);
-			}
-		}
-	}
+	function processMessage(msg) {
+        var message = msg.content.toLowerCase()
+        var channelId = msg.channel.id
+        var channel = client.channels.cache.get(channelId)
+        var dicePattern = CommandStore.commands.dice.regexPattern //search for '#d#' expression.
+
+        let isBot = msg.author.isBot
+        //command and message handling block
+        //commands that look for keyword
+        if (message.startsWith(keyword) && !isBot) {
+            message = message.substring(2) //extracts the message without the "r!"
+            if (message === commands.beer.name) {
+                commands.beer.main(channel)
+            } else if (message === commands.flops.name) {
+                commands.flops.main(channel)
+            } else if (message === commands.drink.name) {
+                commands.drink.main(channel)
+            } else if (dicePattern.test(message)) {
+                commands.dice.main(channel, message)
+            } else if (message === commands.help.name) {
+                var helpBox = new Discord.MessageEmbed()
+                commands.help(channel, helpBox)
+            }
+        }
+        //commands that do NOT look for keyword
+        else if (!isBot) {
+            if (commands.caCourt.caMarche(message)) {
+                commands.caCourt.court(channel)
+            }
+            if (commands.notch.notchable(message)) {
+                commands.notch.main(channel)
+            }
+            if (commands.huh.canHuh(message)) {
+                commands.huh.main(channel, message)
+            }
+        }
+    }
 
 client.login(token) //bot token --> should NOT be commited and made public.
--- src/commands/dice.js
+++ src/commands/dice.js
@@ -1,20 +1,18 @@
 module.exports = {
     main: (channel, msg, modifiers) => {
         var xy = msg.split('d')
-		var numDice = parseInt(xy[0])
-		var numSides = parseInt(xy[1])
-
-		for (let i = 0; i < numDice; i++) {
-			channel.send('rolling dice ' + (i + 1))
-			channel.send('.\n.\n.');
-			// To add a bit of suspense lol
-			setTimeout(() => {
-				// + 1 because dices should start at 1 not 0
-				channel.send('Result: ' + Math.floor(Math.random() * numSides + 1));
-			});
-		}
-	},
+        var numDice = parseInt(xy[0])
+        var numSides = parseInt(xy[1])
+		
+        for (let i = 0; i < numDice; i++) {
+            channel.send('Rolling dice (evan\'s test) ' + (i + 1))
+            channel.send('.\n.\n.(evan\'s test)')
+            // + 1 because dices should start at 1 not 0
+            channel.send('Result (evan\'s test)' + Math.floor(Math.random() * numSides + 1))
+        }
+    },
     name: 'dice',
     type: 'active',
-	desc: ' - r!xdy \t--> roll x# of y-sided dice'
+    desc: ' - r!xdy \t--> roll x# of y-sided dice',
+    regexPattern: /\d+[d]\d+/i,
 }
