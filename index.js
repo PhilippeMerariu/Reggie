f274a92aed02e654478d21f4f9e0e1b9829824d3
Ian Phillips <ianjhphillips@gmail.com>
2020-10-20 23:24:19 -0400

    Added argument handling

--- index.js
+++ index.js
@@ -42,45 +42,53 @@ client.on('message', processMessage);
 	 * @param msg
 	 */
 	function processMessage (msg) {
-		var message = msg.content.toLowerCase();
-		var channelId = msg.channel.id;
-		var channel = client.channels.cache.get(channelId);
-		
-		var dicePattern = new RegExp('\\d+[d]\\d+'); //search for '#d#' expression.
-		
+		let messageContent = msg.content.toLowerCase();
+		let channelId = msg.channel.id;
+		let channel = client.channels.cache.get(channelId);
 		let isBot = msg.author.bot;
+
+		let dicePattern = new RegExp('\\d+[d]\\d+'); //search for '#d#' expression.
+		
+
+
 		//command and message handling block
 		//commands that look for keyword
-		if (message.startsWith(keyword) && !isBot){
-			message = message.substring(2); //extracts the message without the "r!"
-			if (message === commands.beer.name) {
+		if (messageContent.startsWith(keyword) && !isBot) {
+
+			messageContent = messageContent.substring(2); //extracts the message without the "r!"
+
+			let messageWords = messageContent.trim().split(' ');
+			let command = messageWords[0];
+			let args = messageWords.slice(1, messageWords.length);
+
+			if (command === commands.beer.name) {
 				commands.beer.main(channel);
 			}
-			else if (message === commands.flops.name) {
+			else if (command === commands.flops.name) {
 				commands.flops.main(channel);
 			}
-			else if (message === commands.drink.name){
+			else if (command === commands.drink.name){
 				commands.drink.main(channel);
 			}
 			else if (dicePattern.test(message)){
 				commands.dice.main(channel, message);
 			}
-			else if (message === commands.help.name){
-				var helpBox = new Discord.MessageEmbed();
+			else if (command === commands.help.name){
+				let helpBox = new Discord.MessageEmbed();
 				commands.help(channel, helpBox);
 				
 			}
 		}
 		//commands that do NOT look for keyword
 		else if (!isBot) {
-			if (commands.caCourt.caMarche(message)) {
+			if (commands.caCourt.caMarche(messageContent)) {
 				commands.caCourt.court(channel);
 			}
-			if (commands.notch.notchable(message)) {
+			if (commands.notch.notchable(messageContent)) {
 				commands.notch.main(channel);
 			}
-			if (commands.huh.canHuh(message)) {
-				commands.huh.main(channel, message);
+			if (commands.huh.canHuh(messageContent)) {
+				commands.huh.main(channel, messageContent);
 			}
 		}
 	}
