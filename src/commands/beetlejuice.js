39be31992c00b1b2ebbe83f3670087a1ab13503a
Philippe Merariu <lmerariu@gmail.com>
2020-11-04 11:20:57 -0500

    Removed test console.log

--- src/commands/beetlejuice.js
+++ src/commands/beetlejuice.js
@@ -6,10 +6,10 @@ module.exports = {
     },
     juicable: (msg) => {
         juice = new RegExp('(beetlejuice(\\s)*){3}');
-        console.log(juice.test(msg));
+        //console.log(juice.test(msg));
         return (juice.test(msg))
     },
     name: 'beetlejuice',
     type: 'passive',
-    desc: ' - beetle juice 3 times \t--> replies with bri-beetlejuice image.'
+    desc: ' - beetlejuice 3 times \t--> replies with bri-beetlejuice image.'
 }
