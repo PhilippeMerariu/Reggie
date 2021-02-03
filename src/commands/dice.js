5edf71a284f5470c500f27e0ec2c913adbe82705
Philippe Merariu <lmerariu@gmail.com>
2021-02-02 22:31:46 -0500

    dice command sends 1 long message. avoids cooldown.

--- src/commands/dice.js
+++ src/commands/dice.js
@@ -3,32 +3,34 @@ module.exports = {
         let xy = msg.split('d')
         let numDice = parseInt(xy[0])
         let numSides = parseInt(xy[1])
-        let max = 20
+        let max = 50
+        let values = [];
         let total = 0;
+        let message = "";
+        let editMessage = "";
 
-        const rollDice = async () => {
-            for (let i = 0; i < numDice; i++) {
-                if (i > max)
-                    break;
-                await channel.send("Rolling dice " + (i + 1) + " --> :game_die:")
-                    .then((m) => {
-                        let res = Math.floor(Math.random() * numSides + 1);
-                        total += res;
-                        setTimeout(() => {
-                            m.edit("Rolling dice " + (i + 1) + " --> " + res);
-                        }, 2000);
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
-            }
+        if (numDice > max){
+            numDice = max;
         }
+        for (let i = 1; i <= numDice; i++){
+            let val = Math.floor(Math.random() * numSides + 1);
+            values.push(val);
+            total += val;
+            message += "Rolling dice " + (i) + " --> :game_die:\n";
+            editMessage += "Rolling dice " + (i) + " --> " + val + "\n";
+        }
+        message += ":game_die: Total score: " + ":pencil:" + " :game_die:";
+        editMessage += ":game_die: Total score: " + total + " :game_die:";
 
-        rollDice().then(() => {
-            setTimeout(() => {
-                channel.send(":game_die: Total score: " + total + " :game_die:");
-            }, 2500);          
-        });
+        channel.send(message)
+            .then((m) => {
+                setTimeout(() => {
+                    m.edit(editMessage);
+                }, 2000);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     },
     name: 'dice',
     type: 'active',
